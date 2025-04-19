76f77e68eaca7392fe6123c3eda0bffd4433201d
Juan Camilo Zuñiga Zuñiga <146753356+JCzunzun@users.noreply.github.com>
2025-04-19 12:05:36 +0200

    pruebas de lint en CI para comienzo de desarrollo

--- src/empty/data/remote/empty.service.ts
+++ src/empty/data/remote/empty.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@nestjs/common';
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { CreateEmptyDto } from '../../presentation/dto/create-empty.dto';
 import { UpdateEmptyDto } from '../../presentation/dto/update-empty.dto';
-/* eslint-enable @typescript-eslint/no-unused-vars */
 
 @Injectable()
-export class EmptyService {  create(createEmptyDto: CreateEmptyDto) {
-    // Parameter is required by interface but not used in the implementation
+export class EmptyService {  
+  create(createEmptyDto: CreateEmptyDto) {
+    console.log('Processing:', createEmptyDto);
     return 'This action adds a new empty';
   }
 
   findAll() {
     return `This action returns all empty`;
-  }
-  findOne(id: number) {
+  }  findOne(id: number) {
     return `This action returns a #${id} empty`;
-  }  update(id: number, updateEmptyDto: UpdateEmptyDto) {
-    // Parameter is required by interface but not used in the implementation
+  }  
+  
+  update(id: number, updateEmptyDto: UpdateEmptyDto) {
+    console.log('Updating with:', updateEmptyDto);
     return `This action updates a #${id} empty`;
   }
 
